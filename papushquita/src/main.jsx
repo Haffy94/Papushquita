209bbd122d2111e924df77955f73e801dc6fd836
Haffy94 <lucasezequielhafner@gmail.com>
2022-11-06 16:27:49 -0300

    primer push login

---
+++ papushquita/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HelloWordApp, LoginPapushquita } from './01-mainMenu/MainMenu'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LoginPapushquita />
+  </React.StrictMode>
+)
