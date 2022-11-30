5b1348abfa46b652ed98ea08c6b72479ecd2eeef
Haffy94 <lucasezequielhafner@gmail.com>
2022-11-30 18:32:36 -0300

    reset

--- papushquita/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { HelloWordApp, LoginPapushquita } from './01-mainMenu/MainMenu'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <LoginPapushquita />
-  </React.StrictMode>
-)
